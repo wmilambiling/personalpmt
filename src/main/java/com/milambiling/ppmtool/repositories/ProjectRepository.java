9643554863a8a947a50d04e49e0e49c79ddf810f
Wilson Milambiling <wmilambiling@users.noreply.github.com>
2019-10-07 06:17:07 +0800

    Added Project Repository

---
+++ src/main/java/com/milambiling/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.milambiling.ppmtool.repositories;
+
+import com.milambiling.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
